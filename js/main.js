2b4d4fca74f479240f2d62970b0c77ec6e687da1
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-08 15:50:08 +0100

    stampo in console e stampo in pagina le stringhe

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@ Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
+
 MILESTONE 1:
 Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
+
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
@@ -17,17 +20,10 @@ Ragioniamo come sempre a step.
 Prima la logica in italiano e poi traduciamo in codice.
 */
 
-/*Wayne Barnett	Founder & CEO				wayne-barnett-founder-ceo.jpg
-Angela Caroll	Chief Editor				angela-caroll-chief-editor.jpg
-Walter Gordon	Office Manager				walter-gordon-office-manager.jpg
-Angela Lopez	Social Media Manager		angela-lopez-social-media-manager.jpg
-Scott Estrada	Developer					scott-estrada-developer.jpg
-Barbara Ramos	Graphic Designer			barbara-ramos-graphic-designer.jpg*/
 
 const arrayObject = document.getElementById("team")
-arrayObject.innerHTML =""
 
-const membriTeam = [
+const team = [
     {
         "nome": "Wayne Barnett",
         "ruolo": "Founder & CEO",
@@ -35,32 +31,44 @@ const membriTeam = [
     },
 
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor",
+        "foto": "angela-caroll-chief-editor.jpg"
     },
 
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "foto": "walter-gordon-office-manager.jpg"
     },
 
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "foto": "angela-lopez-social-media-manager.jpg"
     },
 
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "foto": "scott-estrada-developer.jpg"
     },
 
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "foto": "barbara-ramos-graphic-designer.jpg"
     }
 ]
+//stampo in console
+for (let key in team) {
+    console.log(team[key]);
+}
+
+//stampo in pagina
+for (let i = 0; i < team.length; i++) {
+    let key = team[i];
+    let keyElement = document.createElement("div");
+    keyElement.innerHTML = key.nome + " " + key.ruolo + " " + key.foto + " " ;
+    arrayObject.append(keyElement);
+}
